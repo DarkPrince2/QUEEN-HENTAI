d57e6668a052c6ce3700c71588407ef21cce3a5b
DarkPrince2 <106710231+DarkPrince2@users.noreply.github.com>
2024-03-13 16:00:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['263714121326', 'Dinuwa Official²⁰²³', true],
+  ['26374121326', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
